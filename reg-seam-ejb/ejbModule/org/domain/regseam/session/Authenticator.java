4f4ab1850c441ac5b84a93f5fe6f05034a54f53d
Misiek <misiek_gda@o2.pl>
2010-11-08 13:32:08 +0100

    poprawki css

--- reg-seam-ejb/ejbModule/org/domain/regseam/session/Authenticator.java
+++ reg-seam-ejb/ejbModule/org/domain/regseam/session/Authenticator.java
@@ -1,32 +1,46 @@
 package org.domain.regseam.session;
 
+import javax.persistence.EntityManager;
+import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.log.Log;
+import org.jboss.seam.annotations.Out;
 import org.jboss.seam.security.Credentials;
 import org.jboss.seam.security.Identity;
+import org.jboss.seam.log.Log;
+
+import example.regseam.entity.Osoba;
 
 @Name("authenticator")
-public class Authenticator
-{
-    @Logger private Log log;
+public class Authenticator {
 
-    @In Identity identity;
-    @In Credentials credentials;
+	@Logger private Log log;
 
-    public boolean authenticate()
-    {
-        log.info("authenticating {0}", credentials.getUsername());
-        //write your authentication logic here,
-        //return true if the authentication was
-        //successful, false otherwise
-        if ("admin".equals(credentials.getUsername()))
-        {
-            identity.addRole("admin");
-            return true;
+	@In Identity identity;
+	@In Credentials credentials;
+	
+	@Out (scope = ScopeType.SESSION, required = false) Osoba loggedIn; 
+
+	
+	private EntityManager entityManager;
+	public boolean authenticate() {
+		
+		log.info("authenticating {0}", credentials.getUsername());
+
+        Osoba osoba = (Osoba)entityManager.createQuery("select osoba from Osoba osoba where osoba.login = :username").
+        		setParameter("username", credentials.getUsername()).
+        	getSingleResult();
+        if (null!=osoba) {
+        	if (osoba.getHaslo().equals(credentials.getPassword())) {
+        		loggedIn = osoba;
+        		
+        		//identity.addRole(osoba.getRole());
+        		
+        		return true;
+        	}
         }
         return false;
-    }
 
+	}
 }
