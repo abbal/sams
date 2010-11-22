a0adae43402ea9ddf63229cd9be55bccccdb8d7a
Misiek <misiek_gda@o2.pl>
2010-11-22 02:38:25 +0100

    poprawka do poprzedniego commita

--- reg-seam-ejb/ejbModule/example/regseam/entity/Dziekanat.java
+++ reg-seam-ejb/ejbModule/example/regseam/entity/Dziekanat.java
@@ -7,16 +7,4 @@ import javax.persistence.Entity;
 @DiscriminatorValue("D")
 public class Dziekanat extends Osoba {
 private static final long serialVersionUID = 1L;
-	
-private long id;
-
-public long getId() {
-	return id;
-}
-
-public void setId(long id) {
-	this.id = id;
-}
-	
-
 }
